bff7b7eb89827bb33a3e31205b690d6f0d706cca
Justo Miko Marquez <marquezmiko@gmail.com>
2022-08-17 23:28:31 -0500

    Setup jest and react testing library for Overview component

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const webpack = require('webpack');
+const dotEnv = require('dotenv-webpack');
 
 module.exports = {
   mode: "development",
@@ -19,9 +21,25 @@ module.exports = {
               "@babel/preset-env",
               "@babel/preset-react"
             ],
-          }
+          },
         },
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+        options: { presets: ['@babel/env', '@babel/preset-react'] },
       },
     ],
   },
+  plugins: [
+    // new webpack.DefinePlugin({
+    //   'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+    //   'process.env.DEBUG': JSON.stringify(process.env.DEBUG),
+    // })
+    new dotEnv({
+      ignoreStub: true,
+    })
+  ]
 };
+
