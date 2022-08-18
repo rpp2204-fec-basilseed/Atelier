9b6bd9620f2501e1278bb6c9dfd717eec6bf66a8
Keenan Aldridge <kmantan@gmail.com>
2022-08-18 13:42:46 -1000

    Completed barebones skeleton for ratings & reviews

--- client/src/component/Reviews.jsx
+++ client/src/component/Reviews.jsx
@@ -4,7 +4,7 @@ export default function Reviews () {
   return (
   <div>
     <h2>Ratings and Reviews</h2>
-    <div className='main'>
+    <div className='main' style={{display: 'flex'}}>
       <div className='ratings'>
         <div className='stars'>3.5</div>
         <div className='stars-breakdown'>5 4 3 2 1</div>
@@ -12,7 +12,8 @@ export default function Reviews () {
         <div className='comfort-rating-slide'>Comfort rating goes here</div>
       </div>
       <div className='reviews'>
-        <div className='total-reviews'>Total reviews with sort function goes here</div>
+        <div className='total-reviews'>Total reviews with 'sort by' function goes here</div>
+        <div className='review'>Review Goes Here</div>
       </div>
     </div>
   </div>
