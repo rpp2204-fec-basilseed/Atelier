3f22d4fdb18df6177fb3a90a8b998d186330b8bc
Justo Miko Marquez <marquezmiko@gmail.com>
2022-08-18 23:23:56 -0500

    Added dynamic population of product image, name, price

--- client/src/component/Overview.jsx
+++ client/src/component/Overview.jsx
@@ -1,26 +1,59 @@
 import React from 'react';
+const axios = require('axios');
 
 class Overview extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {}
+    this.state = {
+      currProdData: [],
+      selectedStyle: ''
+    }
+  }
+
+  componentDidMount() {
+    var config = {
+      method: 'get',
+      url: 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-rpp/products/'+this.props.curr_product_id+'/styles',
+      headers: {
+        'Authorization': process.env.REACT_APP_API_KEY
+      }
+    };
+
+    axios(config)
+    .then((response) => {
+      console.log(JSON.stringify(response.data));
+      this.setState({
+        currProdData: response.data.results,
+        selectedStyle: 0
+      });
+    })
+    .catch((error) => {
+      console.log(error);
+    });
   }
 
   render() {
+    var currProdImage = <div>Product Image</div>;
+    var currProdName = <div>Product Name</div>;
+    var currProdPrice = <div>Product Price</div>;
+    if (this.state.currProdData.length !== 0) {
+      currProdImage = <img src={this.state.currProdData[this.state.selectedStyle].photos[0].url} alt="main product photo" />;
+      currProdName = <div>{this.state.currProdData[this.state.selectedStyle].name}</div>
+      currProdPrice = <div>${this.state.currProdData[this.state.selectedStyle].original_price}</div>
+    }
     return (
       <div className="overview-container">
-        Overview
+        {currProdImage}
+        {currProdName}
+        {currProdPrice}
         <div>
-          Primary photo
+          Style selection component
         </div>
         <div>
-          Description
+          Size selection component
         </div>
         <div>
-          Style selection
-        </div>
-        <div>
-          Cart/Favorite
+          Cart/Favorite component
         </div>
         <div>
           Product info
