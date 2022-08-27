f05b549bd08677245ffe5fe560d7908ad03afff3
Keenan Aldridge <kmantan@gmail.com>
2022-08-27 08:28:32 -1000

    Further cleaned up file structure

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import * as ReactDOM from 'react-dom/client';
-import Reviews from './components/Reviews.jsx';
+import Reviews from './components/RatingsAndReviews/Reviews.jsx';
 const Axios = require('axios');
 import { FaStar, FaStarHalfAlt } from 'react-icons/fa';
 import { AiOutlineStar } from 'react-icons/ai';
