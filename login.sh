984f9bc27c538ecd7141a2830610a105cf273b79
Keren Liu <karenliu816@gmail.com>
2022-09-15 21:36:11 -0400

    ready for next pull main

--- login.sh
+++ login.sh
@@ -1,3 +1,3 @@
-ssh -i '/Users/keren.liu/0607 temporary place for recast.ly and Atelier/FEC-Atelier/helloWorld.pem' ubuntu@ec2-3-83-12-131.compute-1.amazonaws.com
+ssh -i '/Atelier/helloWorld.pem' ubuntu@ec2-3-83-12-131.compute-1.amazonaws.com
 
 
